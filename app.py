236c741f94fde5f725a871a305283a0260137a04
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-11-18 23:08:30 +0900

    モデル作成

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello World '
+    return render_template('index.html')
 
 
-app = Flask(__name__)                                                          
-app.run(debug=True)
